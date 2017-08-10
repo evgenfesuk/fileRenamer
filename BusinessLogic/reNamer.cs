74be51dc65b477381d9e86bc57f2303f36e69271
evgen.fesuk <evgen.fesuk@gmail.com>
2017-08-10 14:31:36 +0300

    moving to MVC pattern

---
+++ BusinessLogic/reNamer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace BusinessLogic
+{
+    public class reNamer
+    {
+        private string GetExif(string path)
+        {
+            FileStream Foto = File.Open(path, FileMode.Open, FileAccess.Read); // открыли файл для чтения
+            BitmapDecoder decoder = JpegBitmapDecoder.Create(Foto, BitmapCreateOptions.IgnoreColorProfile, BitmapCacheOption.Default); //"распаковали" снимок и создали объект decoder
+            BitmapMetadata TmpImgEXIF = (BitmapMetadata)decoder.Frames[0].Metadata.Clone(); //считали и сохранили метаданные
+            string fileName = TmpImgEXIF.DateTaken;
+            Foto.Close();
+            return fileName;
+        }
+
+        private string NameChange(string path)
+        {
+            string fileName = GetExif(path) + "(2)" + System.IO.Path.GetExtension(path);
+
+            fileName = MakeGoodName(fileName);
+
+            string fullFileName = System.IO.Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+
+            return fullFileName;
+        }
+
+        private string NameCreate(string path)
+        {
+            string fileName = GetExif(path) + System.IO.Path.GetExtension(path);
+
+            fileName = MakeGoodName(fileName);
+
+            string fullFileName = System.IO.Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+
+            return fullFileName;
+        }
+
+        private void ClosePhoto(string path)
+        {
+            FileStream Foto = File.Open(path, FileMode.Open, FileAccess.Read);
+            Foto.Close();
+        }
+
+        private string MakeGoodName(string fileName)
+        {
+            fileName = fileName.Replace(" ", "_");
+            fileName = fileName.Replace(":", "-");
+            return fileName;
+        }
+
+        public void ReName()
+        {
+            string[] dirs = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.jpg");
+
+            foreach (string path in dirs)
+            {
+                if (!File.Exists(NameCreate(path))) File.Copy(path, NameCreate(path));
+                else
+                {
+                    File.Copy(path, NameChange(path), true);
+                }
+                File.Delete(path);
+            }
+        }
+    }
+}
--- fileRenamer/MainWindow.xaml.cs
+++ fileRenamer/MainWindow.xaml.cs
@@ -17,82 +17,27 @@ using System.Globalization;
 
 namespace fileRenamer
 {
+
+    public interface IMnainForm
+    {
+        event EventHandler onBtnClick;
+    }
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IMnainForm
     {
         public MainWindow()
         {
             InitializeComponent();
+            btnReName.Click += BtnReName_Click;
         }
 
-
-        private void btnReName_Click(object sender, RoutedEventArgs e)
+        private void BtnReName_Click(object sender, RoutedEventArgs e)
         {
-            /*string path = txtPath.Text;
-            string dir = System.IO.Path.GetDirectoryName(path);
-            dir = Directory.GetCurrentDirectory();*/
-
-            string[] dirs = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.jpg");
-
-            foreach (string path in dirs)
-            {
-                if (!File.Exists(NameCreate(path)))  File.Copy(path, NameCreate(path));
-                else
-                {
-                    File.Copy(path, NameChange(path), true);
-                }
-                File.Delete(path);
-            }
-
+            if (onBtnClick != null) onBtnClick(this, EventArgs.Empty);
         }
 
-        private string NameChange(string path)
-        {
-            string fileName = GetExif(path) + "(2)" + System.IO.Path.GetExtension(path);
-
-            fileName = MakeGoodName(fileName);
-
-            string fullFileName = System.IO.Path.Combine(Directory.GetCurrentDirectory(), fileName);
-
-
-            return fullFileName;
-        }
-
-        private string NameCreate(string path)
-        {
-            string fileName = GetExif(path) + System.IO.Path.GetExtension(path);
-
-            fileName = MakeGoodName(fileName);
-
-            string fullFileName = System.IO.Path.Combine(Directory.GetCurrentDirectory(), fileName);
-
-
-            return fullFileName;
-        }
-
-        private string GetExif(string path)
-        {
-            FileStream Foto = File.Open(path, FileMode.Open, FileAccess.Read); // открыли файл для чтения
-            BitmapDecoder decoder = JpegBitmapDecoder.Create(Foto, BitmapCreateOptions.IgnoreColorProfile, BitmapCacheOption.Default); //"распаковали" снимок и создали объект decoder
-            BitmapMetadata TmpImgEXIF = (BitmapMetadata)decoder.Frames[0].Metadata.Clone(); //считали и сохранили метаданные
-            string fileName = TmpImgEXIF.DateTaken;
-            Foto.Close();
-            return fileName;
-        }
-
-        private void ClosePhoto(string path)
-        {
-            FileStream Foto = File.Open(path, FileMode.Open, FileAccess.Read);
-            Foto.Close();
-        }
-
-        private string MakeGoodName(string fileName)
-        {
-            fileName = fileName.Replace(" ", "_");
-            fileName = fileName.Replace(":", "-");
-            return fileName;
-        }
+        public event EventHandler onBtnClick;
     }
 }
