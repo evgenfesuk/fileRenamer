0b78ef7373abf29286eda2c7335bad5eea148dd8
evgen.fesuk <evgen.fesuk@gmail.com>
2017-11-05 18:18:30 +0200

    YYYY.MM.DD_TIME именование

--- Exif/GetExif.cs
+++ Exif/GetExif.cs
@@ -10,7 +10,7 @@ namespace Exif
             FileStream Foto = File.Open(path, FileMode.Open, FileAccess.Read); // открыли файл для чтения
             BitmapDecoder decoder = JpegBitmapDecoder.Create(Foto, BitmapCreateOptions.IgnoreColorProfile, BitmapCacheOption.Default); //"распаковали" снимок и создали объект decoder
             BitmapMetadata TmpImgEXIF = (BitmapMetadata)decoder.Frames[0].Metadata.Clone(); //считали и сохранили метаданные
-            string fileName = TmpImgEXIF.DateTaken;
+            string fileName = TmpImgEXIF.DateTaken.Substring(6, 4) + "." + TmpImgEXIF.DateTaken.Substring(3, 3) + TmpImgEXIF.DateTaken.Substring(0, 2) + TmpImgEXIF.DateTaken.Substring(10); ;
             Foto.Close();
             return fileName;
         }
