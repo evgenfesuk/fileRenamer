5f7daa6f3fe0a7ac5044231af5c22f5dd8f7d02c
evgen.fesuk <evgen.fesuk@gmail.com>
2017-11-19 20:02:09 +0200

    папки создаются по месяцам с названиями

--- Logic/Folder.cs
+++ Logic/Folder.cs
@@ -45,9 +45,20 @@ namespace Logic
                     _month = Path.GetFileNameWithoutExtension(path).Substring(5, 2);
                     switch(_month)
                     {
-                        case "01": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(5, 2) + "\\");
+                        case "01": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\01 Январь\\"); break;
+                        case "02": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\02 Февраль\\"); break;
+                        case "03": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\03 Март\\"); break;
+                        case "04": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\04 Апрель\\"); break;
+                        case "05": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\05 Май\\"); break;
+                        case "06": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\06 Июнь\\"); break;
+                        case "07": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\07 Июль\\"); break;
+                        case "08": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\08 Август\\"); break;
+                        case "09": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\09 Сентябрь\\"); break;
+                        case "10": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\10 Октябрь\\"); break;
+                        case "11": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\11 Ноябрь\\"); break;
+                        case "12": dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\12 Декабрь\\"); break;
+                        default: dn = dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\"); break;
                     }
-                    dn = Path.Combine(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(0, 4) + "\\" + Path.GetFileNameWithoutExtension(path).Substring(5, 2) + "\\");
                     if (!Directory.Exists(dn)) Directory.CreateDirectory(dn);
                     FileInfo f = new FileInfo(path);
                     f.MoveTo(dn + Path.GetFileName(path));
