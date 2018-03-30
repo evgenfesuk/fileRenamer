79082c96677261096db050e3f675fce697313887
evgen.fesuk <evgen.fesuk@gmail.com>
2018-03-30 19:41:32 +0300

    UnitTest

---
+++ fileRenamer.Unit.Test/FileCreateTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fileRenamer.Unit.Test
+{
+    [TestClass]
+    public class FileCreateTest
+    {
+        [TestMethod]
+        public void CreatingFileWithPath()
+        {
+            string _path = "D:\\test.jpg";
+            Name.FileCreate(_path);
+        }
+    }
+}
---
+++ fileRenamer.Unit.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("fileRenamer.Unit.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fileRenamer.Unit.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aafff33d-8d85-4d13-9e1c-5d4cb24bfd7a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
